e9f04187c80d1b61fa3c6ede0065357c2ccc9b08
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-01 20:48:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,9 +28,9 @@ def monopoly_with_fourth_tier
 		pieces: 4,
 		rent_in_dollars: {four_pieces_owned: 200, one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100},
 		names: {
-			reading_railroad: {"mortgage_value" => "$100"}, 
-			pennsylvania_railroad: {"mortgage_value" => "$200"}, 
-			b_and_o_railroad: {"mortgage_value" => "$400"}, 
+			reading_railroad: {"mortgage_value" => "$100"},
+			pennsylvania_railroad: {"mortgage_value" => "$200"},
+			b_and_o_railroad: {"mortgage_value" => "$400"},
 			shortline_railroad: {"mortgage_value" => "$800"}
 		}
 
