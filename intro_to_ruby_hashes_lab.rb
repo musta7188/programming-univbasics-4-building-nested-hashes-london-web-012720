e950d8a3fccd26ba78cb19e291ac2c12a8b96530
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-01 20:42:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
 	{railroads: {
 		pieces: 4,
 		rent_in_dollars: {four_pieces_owned: 200, one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100},
-		names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, key4: ""}
+		names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
 
 		}}
 end
